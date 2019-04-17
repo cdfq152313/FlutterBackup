020d70f517f63411b99ddc71ebf56fa63c9c3d50
Denny Cheng <denny@velodash.co>
2019-04-17 17:33:57 +0800

    ORM DEMO

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,104 @@
-import 'package:flutter/material.dart';
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-void main() => runApp(MyApp());
+import 'package:flutter/widgets.dart';
+import 'package:jaguar_query_sqflite/jaguar_query_sqflite.dart';
+import 'package:orm_demo/model/post.dart';
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+/// The adapter
+SqfliteAdapter _adapter;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+void main() async {
+  var sb = StringBuffer();
+  sb.writeln("Jaguar ORM showcase:");
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  sb.writeln('--------------');
+  sb.write('Connecting ...');
+  var dbPath = await getDatabasesPath();
+  _adapter = new SqfliteAdapter(path.join(dbPath, "test.db"));
+  await _adapter.connect();
+  sb.writeln(' successful!');
+  sb.writeln('--------------');
 
-  final String title;
+  final bean = PostBean(_adapter);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  await bean.drop();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  sb.write('Creating table ...');
+  await bean.createTable();
+  sb.writeln(' successful!');
+  sb.writeln('--------------');
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-    print("Hello world");
-  }
+  // Delete all
+  sb.write('Removing old rows (if any) ...');
+  await bean.removeAll();
+  sb.writeln(' successful!');
+  sb.writeln('--------------');
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  // Insert some posts
+  sb.writeln('Inserting sample rows ...');
+  int id1 = await bean
+      .insert(new Post.make(1, 'Coffee?', 4.5, false, DateTime.now()));
+  sb.writeln('Inserted successfully row with id: $id1!');
+  int id2 =
+      await bean.insert(new Post.make(2, 'Sure!', 5.0, true, DateTime.now()));
+  sb.writeln('Inserted successfully row with id: $id2!');
+/*
+  int id3 =
+      await bean.insert(new Post.make(3, null, 5.0, true, DateTime.now()));
+  sb.writeln('Inserted successfully row with id: $id3!');
+  */
+  sb.writeln('--------------');
+
+// Find one post
+  sb.writeln('Reading row with id $id1 ...');
+  Post post1 = await bean.find(id1);
+  sb.writeln(post1);
+  sb.writeln('--------------');
+
+// Find all posts
+  sb.writeln('Reading all rows ...');
+  List<Post> posts = await bean.getAll();
+  posts.forEach((p) => sb.writeln(p));
+  sb.writeln('--------------');
+
+// Update a post
+  sb.write('Updating a column in row with id $id1 ...');
+  await bean.updateReadField(id1, true);
+  sb.writeln(' successful!');
+  sb.writeln('--------------');
+
+// Find one post
+  sb.writeln('Reading row with $id1 to check the update ...');
+  post1 = await bean.find(id1);
+  sb.writeln(post1);
+  sb.writeln('--------------');
+
+  sb.writeln('Removing row with id $id1 ...');
+  await bean.remove(id1);
+  sb.writeln('--------------');
+
+// Find all posts
+  sb.writeln('Reading all rows ...');
+  posts = await bean.getAll();
+  posts.forEach((p) => sb.writeln(p));
+  sb.writeln('--------------');
+
+  sb.writeln('Removing all rows ...');
+  await bean.removeAll();
+  sb.writeln('--------------');
+
+  sb.write('Closing the connection ...');
+  await _adapter.close();
+  sb.writeln(' successful!');
+  sb.writeln('--------------');
+
+  print(sb.toString());
+
+  runApp(SingleChildScrollView(
+      child: Text(sb.toString(), textDirection: TextDirection.ltr)));
 }
